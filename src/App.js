29f6e9c8918a0b70c06d3f7dd1aeba56d08dace2
sangamone <98866361+snehams29@users.noreply.github.com>
2023-02-16 23:30:13 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import IncDecCounter from "./IDcounter/IncDecCounter";
+function App() {
+
+  return (
+    <IncDecCounter />
+  );
+  }
+
+export default App;
